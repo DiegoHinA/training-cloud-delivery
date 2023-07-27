aflp91/microblog

Chapter 21: User Notifications (v0.21)

app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from hashlib import md5
+import json
 from time import time
 from flask import current_app
 from flask_login import UserMixin
@@ -65,6 +66,15 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    messages_sent = db.relationship('Message',
+                                    foreign_keys='Message.sender_id',
+                                    backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message',
+                                        foreign_keys='Message.recipient_id',
+                                        backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -114,6 +124,17 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time).count()
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 @login.user_loader
 def load_user(id):
@@ -134,3 +155,25 @@ class Post(SearchableMixin, db.Model):
 
 db.event.listen(db.session, 'before_commit', Post.before_commit)
 db.event.listen(db.session, 'after_commit', Post.after_commit)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))

DiegoHinA/training-cloud-delivery

app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from hashlib import md5
+import json
 from time import time
 from flask import current_app
 from flask_login import UserMixin
@@ -79,6 +80,16 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.followed_id == id),
         secondaryjoin=(followers.c.follower_id == id),
         lazy='dynamic', back_populates='following')
+    messages_sent = db.relationship('Message',
+                                    foreign_keys='Message.sender_id',
+                                    lazy='dynamic', back_populates='author')
+    messages_received = db.relationship('Message',
+                                        foreign_keys='Message.recipient_id',
+                                        lazy='dynamic',
+                                        back_populates='recipient')
+    last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', lazy='dynamic',
+                                    back_populates='user')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -125,6 +136,17 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time).count()
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 @login.user_loader
 def load_user(id):
@@ -142,3 +164,30 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author = db.relationship('User', foreign_keys='Message.sender_id',
+                             back_populates='messages_sent')
+    recipient = db.relationship('User', foreign_keys='Message.recipient_id',
+                                back_populates='messages_received')
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+    user = db.relationship('User', back_populates='notifications')
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))